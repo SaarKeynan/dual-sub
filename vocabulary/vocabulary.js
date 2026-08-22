a5c5b1368478da32f8063d3323f493b01a0b2903
DualSub Development <dualsub@local>
2026-08-22 17:27:00 +0300

    fix: harden Firefox vocabulary exports

--- vocabulary/vocabulary.js
+++ vocabulary/vocabulary.js
@@ -219,7 +219,9 @@ function downloadFile(name, type, content) {
   const link = document.createElement("a");
   link.href = url;
   link.download = name;
+  document.body.appendChild(link);
   link.click();
+  link.remove();
   setTimeout(() => URL.revokeObjectURL(url), 1000);
 }
 
